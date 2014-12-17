ff0407ed64401b76bea0488ce2441173aace84fb
Steve Gentile <steven.gentile@gmail.com>
2014-12-17 16:32:31 -0500

    missing labelCase fix

--- dist/sds-angular-controls.js
+++ dist/sds-angular-controls.js
@@ -181,6 +181,19 @@ angular.module('sds-angular-controls', ['ui.bootstrap', 'toggle-switch']);
     }).directive('formField', formField);
 })();
 
+(function (){
+  'use strict';
+
+  function labelCase (){
+    return function (input) {
+      input = input.replace(/([A-Z])/g, ' $1');
+      return input[0].toUpperCase() + input.slice(1);
+    };
+  }
+
+  angular.module('sds-angular-controls').filter('labelCase', labelCase);
+})();
+
 angular.module('sds-angular-controls').run(['$templateCache', function($templateCache) {
   'use strict';
 
