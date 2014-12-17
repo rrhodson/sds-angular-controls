52427a474c416d0a2d5df573f2da5f381076f303
Steve Gentile <steven.gentile@gmail.com>
2014-12-17 16:31:10 -0500

    added missing label case

---
+++ src/labelCase.js
@@ -0,0 +1,15 @@
+/**
+ * Created by stevegentile on 12/17/14.
+ */
+(function (){
+  'use strict';
+
+  function labelCase (){
+    return function (input) {
+      input = input.replace(/([A-Z])/g, ' $1');
+      return input[0].toUpperCase() + input.slice(1);
+    };
+  }
+
+  angular.module('sds-angular-controls').filter('labelCase', labelCase);
+})();
