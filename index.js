daae3f195cf63cf715fb6bbfcb7c4143b0722017
Steve Gentile <steven.gentile@gmail.com>
2015-11-24 09:16:17 -0500

    fix for npm packages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by stevegentile on 11/22/15.
  */
 require('angular-ui-bootstrap');
-require('ngSanitize');
+require('angular-sanitize');
 require('./dist/sds-angular-controls.js');
 require('./dist/sds-angular-controls.css');
 module.exports = 'sds-angular-controls';
